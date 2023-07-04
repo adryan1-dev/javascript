a3855b7dd2559948df35fb9b9503dd0c2654e170
Adryan <130511954+serrot-dev@users.noreply.github.com>
2023-07-03 21:27:07 -0300

    movi os arquivos da pasta

---
+++ exercicios/aula11/condicoes1.js
@@ -0,0 +1,6 @@
+var vel = 80.6
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60){
+    console.log('Sua velocidade ultrapassou o limite. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de seegurança!')
